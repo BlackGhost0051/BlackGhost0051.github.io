aacbb6b08149c3b3fc22714723b7ac8ca5b7fbc1
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-03-23 23:00:03 +0100

    Mod structure

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+let navbar = document.querySelector(".nav-bar");
+
+
+let navbarHtml = "" +
+    "        <ul>\n" +
+    "            <li><a href='/index.html'>About Me</a></li>\n" +
+    "            <li><a href='/projects.html'>Projects</a></li>\n" +
+    "            <li><a href='/skills.html'>Skills</a></li>\n" +
+    "            <li><a href='/experience.html'>Experience</a></li>\n" +
+    "            <li><a href='/contact.html'>Contact</a></li>\n" +
+    "        </ul>"
+
+navbar.innerHTML = navbarHtml;
