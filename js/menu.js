b2c9d7f068017bcdb3fed0ab8f5bc90537d766e5
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-04-27 22:04:29 +0200

    Add icons

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,52 @@ let main = document.querySelector(".main");
 let staticContent = document.querySelector(".nav-bar");
 let footer = document.querySelector(".footer");
 
+
+
+
+const links = {
+    "C": "https://www.pngkit.com/png/detail/101-1010012_c-programming-icon-c-programming-language-logo.png",
+    "C++": "https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg",
+    "C#": "https://upload.wikimedia.org/wikipedia/commons/d/d2/C_Sharp_Logo_2023.svg",
+    "Java": "https://e7.pngegg.com/pngimages/123/816/png-clipart-computer-icons-java-%E5%92%96%E5%95%A1%E6%B5%B7%E6%8A%A5%E5%9B%BE%E7%89%87%E7%B4%A0%E6%9D%90-miscellaneous-text-thumbnail.png",
+    "Kotlin": "https://upload.wikimedia.org/wikipedia/commons/0/06/Kotlin_Icon.svg",
+    "Python": "https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png",
+    "JavaScript": "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+    "TypeScript": "https://upload.wikimedia.org/wikipedia/commons/f/f5/Typescript.svg",
+    "GDScript": "https://godotengine.org/assets/favicon.svg",
+
+    "Flask": "https://flask.palletsprojects.com/en/stable/_static/flask-vertical.png",
+    "Spring": "https://spring.io/favicon.svg?v=96334d577af708644f6f0495dd1c7bc8",
+    "wxWidgets": "https://wxwidgets.org/assets/ico/favicon.ico",
+
+    "SQLite": "https://www.sqlite.org/images/sqlite370_banner.svg",
+    "PostgreSQL": "https://www.postgresql.org/media/img/about/press/elephant.png",
+    "MangoDB": "https://www.mongodb.com/assets/images/global/favicon.ico",
+
+    "Docker": "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/97_Docker_logo_logos-512.png",
+    "WordPress": "https://static-00.iconduck.com/assets.00/wordpress-icon-512x512-pmjhtep2.png",
+
+    "Wireshark": "https://www.kali.org/images/tool-logo-wireshark.svg",
+    "Burpsuite": "https://www.kali.org/tools/burpsuite/images/burpsuite-logo.svg",
+    "Nmap": "https://www.kali.org/images/tool-logo-nmap.svg",
+    "Metasploit": "https://www.kali.org/images/tool-logo-metasploit.svg",
+    "Maltego": "https://www.kali.org/images/tool-logo-maltego.svg",
+    "Ghidra": "https://upload.wikimedia.org/wikipedia/commons/f/f6/Ghidra_logo.svg",
+    "Recon-ng": "https://www.kali.org/tools/recon-ng/images/recon-ng-logo.svg",
+
+    "Kali Linux": "https://www.kali.org/images/kali-logo.svg",
+    "Debian": "https://www.debian.org/Pics/openlogo-50.png",
+    "Windows": "https://assets.ubuntu.com/v1/183c7231-windows_logo.svg",
+};
+
+function createSkillItem(name) {
+    return `<li><img src="${links[name]}" width="50" height="50"/>${name}</li>`;
+}
+
+
+
+
+
 let indexMain = `
             <div>
                 index
@@ -55,63 +101,63 @@ let skillsMain = `
                 <div>
                     <h1>Programming languages:</h1>
                     <ul>
-                        <li>C</li>
-                        <li>C++</li>
-                        <li>C#</li>
-                        <li>Java</li>
-                        <li>Kotlin</li>
-                        <li>Python</li>
-                        <li>JavaScript</li>
-                        <li>TypeScript</li>
-                        <li>GDScript</li>
+                        ${createSkillItem("C")}
+                        ${createSkillItem("C++")}
+                        ${createSkillItem("C#")}
+                        ${createSkillItem("Java")}
+                        ${createSkillItem("Kotlin")}
+                        ${createSkillItem("Python")}
+                        ${createSkillItem("JavaScript")}
+                        ${createSkillItem("TypeScript")}
+                        ${createSkillItem("GDScript")}
                     </ul>
                 </div>
                 
                 <div>
                     <h1>Frameworks:</h1>
                     <ul>
-                        <li>Flask</li>
-                        <li>Spring</li>
-                        <li>wxWidgets</li>
+                        ${createSkillItem("Flask")}
+                        ${createSkillItem("Spring")}
+                        ${createSkillItem("wxWidgets")}
                     </ul>
                 </div>
                 
                 <div>
                     <h1>Databases:</h1>
                     <ul>
-                        <li>SQLite</li>
-                        <li>PostgreSQL</li>
-                        <li>MangoDB</li>
+                        ${createSkillItem("SQLite")}
+                        ${createSkillItem("PostgreSQL")}
+                        ${createSkillItem("MangoDB")}
                     </ul>
                 </div>
                 
                 <div>
                     <h1>Tools:</h1>
                     <ul>
-                        <li>Docker</li>    
-                        <li>WordPress</li>    
+                        ${createSkillItem("Docker")}
+                        ${createSkillItem("WordPress")}
                     </ul>
                 </div>
                 
                 <div>
                     <h1>Security Tools:</h1>
                     <ul>
-                        <li>Wireshark</li>
-                        <li>Burpsuite</li>
-                        <li>Nmap</li>
-                        <li>Metasploit</li>
-                        <li>Maltego</li>
-                        <li>Ghidra</li>
-                        <li>Recon-ng</li>
+                        ${createSkillItem("Wireshark")}
+                        ${createSkillItem("Burpsuite")}
+                        ${createSkillItem("Nmap")}
+                        ${createSkillItem("Metasploit")}
+                        ${createSkillItem("Maltego")}
+                        ${createSkillItem("Ghidra")}
+                        ${createSkillItem("Recon-ng")}
                     </ul>
                 </div>
                 
                 <div>
                     <h1>Operating systems:</h1>
                     <ul>
-                        <li>Kali Linux</li>
-                        <li>Debian</li>
-                        <li>Windows</li>
+                        ${createSkillItem("Kali Linux")}
+                        ${createSkillItem("Debian")}
+                        ${createSkillItem("Windows")}
                     </ul>
                 </div>
             </div>`;
